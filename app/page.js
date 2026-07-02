7b7ce4cb249b8ca8637b2601a054e3acaa1a0892
anjanifoodsandspices-tech <anjanifoodsandspices@gmail.com>
2026-07-02 16:04:51 +0530

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: '3rem', textAlign: 'center', fontFamily: 'sans-serif' }}>
+      <h1>Anjani ExportOS</h1>
+      <p>Your CRM is LIVE! 🚀</p>
+    </div>
+  )
+}
